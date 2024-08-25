891c3dd86e22f00ba6df8dd4338eb866a414165e
Abhay Kekre <abhaykekre9@gmail.com>
2024-08-25 12:14:17 +0530

    first commit

--- app/src/main/java/com/example/loginui/MainActivity.kt
+++ app/src/main/java/com/example/loginui/MainActivity.kt
@@ -4,44 +4,63 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.loginui.ui.theme.LoginUITheme
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LoginUITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+            dashboard()
+        }
+    }
+
+    @Preview
+    @Composable
+    private fun dashboard() {
+        Column(
+            Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .background(color = Color(android.graphics.Color.parseColor("#f8eeec"))),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ConstraintLayout {
+                val (topImg, profile) = createRefs()
+                Box(modifier = Modifier
+                    .fillMaxWidth()
+                    .height(245.dp)
+                    .constrainAs(topImg) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                    }
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Color(android.graphics.Color.parseColor("#EA6D35")),
+                                Color(android.graphics.Color.parseColor("#3b608c")),
+                            )
+                        ), shape = RoundedCornerShape(bottomEnd = 40.dp, bottomStart = 40.dp)
                     )
-                }
+                )
             }
         }
     }
+
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LoginUITheme {
-        Greeting("Android")
-    }
-}
